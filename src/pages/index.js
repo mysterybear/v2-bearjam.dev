747d25d5effef0c1c0d3bd4115f8fce33de3d24d
Tom <tom@bearjam.dev>
2020-04-08 10:45:02 +0100

    improvements to index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,19 +27,19 @@ const IndexPage = () => {
 }
 
 const Headline = () => (
-  <section className="my-3 flex items-center flex-col sm:flex-row">
+  <section className="my-3 flex items-center flex-col sm:flex-row sm:justify-between">
     <motion.div
-      className="w-full mb-4 sm:order-last sm:flex-1"
+      className="w-full mb-4 sm:order-last sm:flex-1 flex justify-end"
       variants={defaultVariants(1)}
     >
       <SvgIsometricOne className="w-full" />
     </motion.div>
     <motion.div
-      className="my-6 text-center sm:flex-1 sm:text-left"
+      className="my-6 text-center sm:flex-1 sm:text-left sm:max-w-xs"
       variants={defaultVariants(0)}
     >
       <h1 className="leading-10">Let’s build lovely web experiences</h1>
-      <p className="leading-6 tracking-wide">You can learn with us by reading our blog, and you can work with us.</p>
+      <p className="leading-6 tracking-wide my-6">You can learn with us by reading our blog, and you can work with us.</p>
       <div className="flex justify-center mt-6 sm:justify-start">
         <ButtonLink to="/blog">Read blog</ButtonLink>
       </div>
